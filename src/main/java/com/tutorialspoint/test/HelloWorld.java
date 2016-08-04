f24d25d375ea486c0858d214bdeb57206d26da97
Gvo3d <denis.yakimov89@gmail.com>
2016-08-04 17:13:04 +0300

    Jsf Message test with ui component message

--- src/main/java/com/tutorialspoint/test/HelloWorld.java
+++ src/main/java/com/tutorialspoint/test/HelloWorld.java
@@ -8,21 +8,12 @@ import javax.faces.bean.RequestScoped;
 @RequestScoped
 public class HelloWorld {
 
-    @ManagedProperty(value="#{message}")
-    private Message messageBean;
 
-    private String message;
 
     public HelloWorld() {
         System.out.println("HelloWorld started!");
     }
     public String getMessage() {
-        if(messageBean != null){
-            message = messageBean.getMessage();
-        }
-        return message;
-    }
-    public void setMessageBean(Message message) {
-        this.messageBean = message;
+        return new String ("home");
     }
 }
