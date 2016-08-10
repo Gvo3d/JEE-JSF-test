fd1891234b017fef9709ddaa5cca7f649b1175e8
Gvo3d <denis.yakimov89@gmail.com>
2016-08-10 16:02:49 +0300

    JSF ui component include

--- src/main/java/com/tutorialspoint/test/UserData.java
+++ src/main/java/com/tutorialspoint/test/UserData.java
@@ -1,11 +1,9 @@
 package com.tutorialspoint.test;
 
 import java.io.Serializable;
-import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
 @ManagedBean(name = "userData", eager = true)
 @SessionScoped
@@ -13,21 +11,21 @@ public class UserData implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    public String data = "1";
-
-    public String getData() {
-        return data;
+    private String name;
+    private String password;
+    public String getName() {
+        return name;
     }
-
-    public void setData(String data) {
-        this.data = data;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public String showResult(){
-        FacesContext fc = FacesContext.getCurrentInstance();
-        Map<String,String> params =
-                fc.getExternalContext().getRequestParameterMap();
-        data =  params.get("username");
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String login(){
         return "result";
     }
 }
